5b658098fa5aaf96daf178742ef892028743f9ef
Legalni <97543155+Legalni@users.noreply.github.com>
2024-01-05 18:29:02 +0100

    An application connected to a mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
