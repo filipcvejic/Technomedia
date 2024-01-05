27bb9e79eb1118b735d42f6cff16f7af7ed2b0e2
Legalni <97543155+Legalni@users.noreply.github.com>
2024-01-05 19:31:36 +0100

    Added login user logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,24 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
+
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
