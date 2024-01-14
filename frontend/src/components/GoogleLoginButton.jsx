e35e2f85b066957c240aee44a4f1fc926301f66f
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-01-14 23:58:07 +0100

    Fixed problem with importing client id from .env in google button

--- frontend/src/components/GoogleLoginButton.jsx
+++ frontend/src/components/GoogleLoginButton.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GoogleLogin } from "react-google-login";
 
 const GoogleLoginButton = ({ onSuccess, onFailure }) => {
-  const clientId = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
   return (
     <GoogleLogin
       clientId={clientId}
       buttonText="Login with Google"
-      // onSuccess={onSuccess}
-      // onFailure={onFailure}
+      onSuccess={onSuccess}
+      onFailure={onFailure}
       cookiePolicy={"single_host_origin"}
     />
   );
