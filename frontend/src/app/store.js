d5a65c14495690a1f14bcc7e6c3f41a79e893781
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-03-14 23:57:34 +0100

    Complete styling of the login screen

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -11,6 +11,7 @@ const store = configureStore({
     userAuth: userAuthReducer,
     userCart: userCartReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
   devTools: true,
 });
 
--- frontend/src/user/screens/LoginScreen.jsx
+++ frontend/src/user/screens/LoginScreen.jsx
@@ -25,7 +25,7 @@ const LoginScreen = () => {
     }
   }, [navigate, userInfo]);
 
-  const google = async () => {
+  const googlePopupWindowHandler = async () => {
     window.open("http://localhost:5000/api/v1/login/google", "_self");
   };
 
@@ -71,11 +71,7 @@ const LoginScreen = () => {
   return (
     <div className="login-container">
       <div className="login-section">
-        <h2 className="login-heading">Registered users</h2>
-        <p className="login-description">
-          If you have an account, sign in using your email address.
-        </p>
-
+        <h1 className="login-heading">Login</h1>
         <form className="login-form" onSubmit={loginHandler}>
           <div className="form-group">
             <label htmlFor="email" className="form-label">
@@ -105,29 +101,56 @@ const LoginScreen = () => {
               required
             />
           </div>
-
-          <div className="actions-toolbar">
-            <button className="submit-button" type="submit">
-              Sign In
-            </button>
-            <div className="forgot-password">
-              <Link to="/forgotpassword">Forgot password?</Link>
-            </div>
+          <div className="forgot-password">
+            <Link to="/forgotpassword">Forgot password?</Link>
           </div>
+          <button className="submit-button" type="submit">
+            Login
+          </button>
         </form>
-      </div>
-      <div className="google-sign-in-container">
-        <button onClick={google}>Google</button>
-      </div>
-      <div className="register-section">
-        <h2 className="register-heading">New user</h2>
-        <p className="register-description">
-          Creating an account on our site is fast and free! Enjoy the benefits
-          of simpler ordering and faster checkout.
-        </p>
-        <Link to="/register" className="register-button">
-          Register
-        </Link>
+        <div className="actions-toolbar">
+          <div className="addition-actions">
+            <p className="register-suggest">
+              Don't have an account? <Link to="/register">Sign up</Link>
+            </p>
+            <span className="or-option">or</span>
+            <div className="external-login-container">
+              <button
+                className="external-login-button"
+                onClick={googlePopupWindowHandler}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 48 48"
+                  width="30px"
+                  height="30px"
+                >
+                  <path
+                    fill="#FFC107"
+                    d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                  />
+                  <path
+                    fill="#FF3D00"
+                    d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                  />
+                  <path
+                    fill="#4CAF50"
+                    d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                  />
+                  <path
+                    fill="#1976D2"
+                    d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                  />
+                </svg>
+                <span>Continue with Google</span>
+              </button>
+            </div>
+            <span className="or-option">or</span>
+            <p className="continue-as-guest">
+              <Link to="/">Continue as Guest</Link>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
