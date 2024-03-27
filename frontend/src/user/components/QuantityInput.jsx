4adc5920f9b6e1771f41b17da27abaf4078abe51
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-03-27 21:35:42 +0100

    Changed some things and completed quantity input component

--- frontend/src/user/components/QuantityInput.jsx
+++ frontend/src/user/components/QuantityInput.jsx
@@ -1,30 +1,60 @@
 import { useDispatch } from "react-redux";
 import "./QuantityInput.css";
+import { addToCart, decreaseProductQuantity } from "../features/cart/cartApi";
 
 export default function QuantityInput({ item }) {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-  
-  const onChangeQuantityHandler = (event) => {
-    if(event.target.value < item.quantity) {
+  const increaseQuantityHandler = (event) => {
+    event.preventDefault();
 
-    }
+    dispatch(
+      addToCart({
+        product: item.product?._id,
+        name: item.product.name,
+        description: item.product.description,
+        price: item.product.price,
+        image: item.product.image,
+        category: item.product.category?._id,
+        subcategory: item.product.subcategory?._id,
+      })
+    );
+  };
+
+  const decreaseQuantityHandler = (event) => {
+    event.preventDefault();
+
+    dispatch(
+      decreaseProductQuantity({
+        productId: item.product?._id,
+      })
+    );
   };
 
   return (
-    <div class="quantity-input">
-      <button class="quantity-count-minus" data-action="minus" type="button">
+    <div className="quantity-input">
+      <button
+        className="quantity-count-minus"
+        data-action="minus"
+        type="button"
+        onClick={decreaseQuantityHandler}
+      >
         -
       </button>
       <input
-        class="item-quantity"
+        className="item-quantity"
         type="number"
         min="0"
         max="100"
         value={item.quantity}
-        onChange={onChangeQuantityHandler}
+        readOnly
       />
-      <button class="quantity-count-plus" data-action="plus" type="button">
+      <button
+        className="quantity-count-plus"
+        data-action="plus"
+        type="button"
+        onClick={increaseQuantityHandler}
+      >
         +
       </button>
     </div>
