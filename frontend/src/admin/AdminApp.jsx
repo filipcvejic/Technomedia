22fe23a5886f4d81a568d9f9e1a88bf32ee49d66
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-04-22 23:50:11 +0200

    Created admin nav menu

--- frontend/src/admin/AdminApp.jsx
+++ frontend/src/admin/AdminApp.jsx
@@ -2,13 +2,16 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "./components/AdminHeader";
+import AdminNavMenu from "./components/AdminNavMenu";
+import "./AdminApp.css";
 
 function AdminApp() {
   return (
     <>
       <AdminHeader />
       <ToastContainer />
-      <div>
+      <div className="admin-main-wrapper">
+        <AdminNavMenu />
         <Outlet />
       </div>
     </>
