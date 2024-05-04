c1787f160ad2e980a35bdda70f70f95350b2136b
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-04 23:58:40 +0200

    Create useCartActions hook with all cart functionalities

---
+++ frontend/src/user/hooks/useCartActions.js
@@ -0,0 +1,68 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToCart,
+  decreaseProductQuantity,
+  removeFromCart,
+} from "../features/cart/cartApi";
+import {
+  addToCartForGuest,
+  decreaseProductQuantityForGuest,
+  removeFromCartForGuest,
+} from "../features/cart/cartSlice";
+
+export function useCartActions() {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.userAuth);
+
+  const addToCartHandler = (product) => {
+    dispatch(
+      userInfo
+        ? addToCart({
+            product: product?._id,
+            name: product.name,
+            description: product.description,
+            price: product.price,
+            images: product.images,
+            category: product.category?._id,
+            subcategory: product.subcategory?._id,
+            group: product.group?._id,
+            brand: product.brand?._id,
+          })
+        : addToCartForGuest({
+            product: {
+              _id: product._id,
+              name: product.name,
+              description: product.description,
+              price: product.price,
+              images: product.images,
+              category: product.category?._id,
+              subcategory: product.subcategory?._id,
+              group: product.group?._id,
+              brand: product.brand?._id,
+            },
+          })
+    );
+  };
+
+  const decreaseProductQuantityHandler = (productId) => {
+    dispatch(
+      userInfo
+        ? decreaseProductQuantity({ productId })
+        : decreaseProductQuantityForGuest({ productId })
+    );
+  };
+
+  const removeFromCartHandler = (productId) => {
+    dispatch(
+      userInfo
+        ? removeFromCart({ productId })
+        : removeFromCartForGuest({ productId })
+    );
+  };
+
+  return {
+    addToCart: addToCartHandler,
+    decreaseProductQuantity: decreaseProductQuantityHandler,
+    removeFromCart: removeFromCartHandler,
+  };
+}
