65e37d0fc11bf4688be571cd5eb410ad2799e60d
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-05 23:53:58 +0200

    Implemented new functionality in mini cart and fixed all bugs

--- frontend/src/user/components/MiniCart.jsx
+++ frontend/src/user/components/MiniCart.jsx
@@ -1,22 +1,13 @@
-import { useDispatch } from "react-redux";
-import { removeFromCart } from "../features/cart/cartApi";
 import QuantityInput from "./QuantityInput";
-import { Link } from "react-router-dom";
 
 import "./MiniCart.css";
 import { useRef } from "react";
+import RemoveFromCartButton from "./RemoveFromCartButton";
 
 function MiniCart({ cart, onOutsideClick }) {
-  const dispatch = useDispatch();
-
   const miniCartRef = useRef(null);
 
   onOutsideClick(miniCartRef);
-
-  const removeItemFromCartHandler = (event, productId) => {
-    event.preventDefault();
-    dispatch(removeFromCart({ productId }));
-  };
 
   let totalAmount = 0;
 
@@ -47,23 +38,7 @@ function MiniCart({ cart, onOutsideClick }) {
                 </span>
                 <div className="single-item-quantity">
                   <QuantityInput item={item} />
-                  <button
-                    className="remove-item-button"
-                    onClick={(event) =>
-                      removeItemFromCartHandler(event, item.product?._id)
-                    }
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="28"
-                      height="28"
-                      fill="black"
-                      className="bi bi-x"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"></path>
-                    </svg>
-                  </button>
+                  <RemoveFromCartButton item={item} />
                 </div>
               </div>
             </div>
