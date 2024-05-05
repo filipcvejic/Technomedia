67d1c4069ad992e8a45d6fd36eb9230a1d35dd17
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-05 23:52:43 +0200

    Updated shorter and better functionality in cart screen and updated some stylings

--- frontend/src/user/screens/CartScreen.jsx
+++ frontend/src/user/screens/CartScreen.jsx
@@ -1,20 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import "./CartScreen.css";
 import QuantityInput from "../components/QuantityInput";
-import { removeFromCart } from "../features/cart/cartApi";
+
+import RemoveFromCartButton from "../components/RemoveFromCartButton";
 
 function CartScreen() {
   const { cart } = useSelector((state) => state.userCart);
 
-  const dispatch = useDispatch();
-
   let totalAmount = 0;
-
-  const removeItemFromCartHandler = (event, productId) => {
-    event.preventDefault();
-
-    dispatch(removeFromCart({ productId }));
-  };
 
   return (
     <div className="cart-container">
@@ -35,23 +28,7 @@ function CartScreen() {
                   <QuantityInput item={item} />
                   <span>{item.product?.price} EUR</span>
                 </div>
-                <button
-                  className="remove-cart-item-button"
-                  onClick={(event) =>
-                    removeItemFromCartHandler(event, item.product?._id)
-                  }
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="28"
-                    height="28"
-                    fill="black"
-                    className="bi bi-x"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                  </svg>
-                </button>
+                <RemoveFromCartButton item={item} />
               </div>
             );
           })}
@@ -67,12 +44,12 @@ function CartScreen() {
             </div>
             <div className="online-discount">
               <p>Discount:</p>
-              <span>0,00 RSD</span>
+              <span>0,00 EUR</span>
             </div>
           </div>
           <div className="purchase-amount">
             <p>Purchase amount:</p>
-            <span>{totalAmount}</span>
+            <span>{totalAmount} EUR</span>
           </div>
           <button className="order-button">Order</button>
         </div>
