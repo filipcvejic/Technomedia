330d549b7ea89960ac3d5e92ff45a51e7785bc89
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-17 23:55:34 +0200

    Made some changes to the structure of the cart model

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -5,10 +5,7 @@ const cartSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
-  },
-  admin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Admin",
+    required: true,
   },
   products: [
     {
@@ -24,14 +21,6 @@ const cartSchema = new Schema({
     },
   ],
 });
-
-cartSchema.path("admin").validate(function (value) {
-  return value || this.user;
-}, "The admin or user must be present.");
-
-cartSchema.path("user").validate(function (value) {
-  return value || this.admin;
-}, "The admin or user must be present.");
 
 const Cart = mongoose.model("Cart", cartSchema);
 
