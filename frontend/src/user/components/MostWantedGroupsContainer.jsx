252fead5ef03550879e359fd6934054ce8405c37
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-17 23:56:08 +0200

    Created most wanted groups container

---
+++ frontend/src/user/components/MostWantedGroupsContainer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Loader from "../../shared/components/Loader";
+import "./MostWantedGroupsContainer.css";
+
+function MostWantedGroupsContainer({ recommendedGroups }) {
+  return (
+    <div className="most-wanted-groups-container">
+      <h1 className="most-wanted-groups-title">The most wanted groups</h1>
+      {recommendedGroups ? (
+        <div className="most-wanted-groups">
+          {recommendedGroups?.map((group, index) => (
+            <Link
+              to={`/${group.category.slug}/${group.subcategory.slug}/${group.slug}`}
+              className="single-category"
+              key={index}
+            >
+              <div className="outer-category-image">
+                <img
+                  src={`/images/category-background-varient${index + 1}.png`}
+                />
+                <div className="inner-category-image">
+                  <img
+                    src={`http://localhost:5000/images/${
+                      group.image.split("\\")[2]
+                    }`}
+                  />
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
+}
+
+export default MostWantedGroupsContainer;
