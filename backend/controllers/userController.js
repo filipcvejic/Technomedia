25bca99541562fa9a6d98c1916dd95dceecf4458
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-18 23:51:21 +0200

    Created logic for recommended products and the most wanted groups

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,6 +11,13 @@ const Product = require("../models/productModel");
 const Category = require("../models/categoryModel");
 const Subcategory = require("../models/subcategoryModel");
 const Group = require("../models/groupModel");
+const Order = require("../models/orderModel");
+const {
+  calculatePriceRange,
+  extractBrands,
+  extractUniqueSubcategories,
+  extractUniqueGroups,
+} = require("../utils/getProductsInfoActions");
 
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -172,11 +179,17 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 
   const updatedUser = await user.save();
 
+  const registerDate = `${updatedUser.createdAt.getDate()}.${
+    updatedUser.createdAt.getMonth() + 1
+  }.${updatedUser.createdAt.getFullYear()}`;
+
+  const adjustedUser = {
+    ...updatedUser.toObject(),
+    createdAt: registerDate,
+  };
+
   res.status(200).json({
-    _id: updatedUser._id,
-    name: updatedUser.name,
-    surname: updatedUser.surname,
-    email: updatedUser.email,
+    user: adjustedUser,
   });
 });
 
@@ -360,7 +373,9 @@ const addProductToCart = asyncHandler(async (req, res, next) => {
       images: addedProduct.images,
       brand: addedProduct.brand,
       category: addedProduct.category,
+      group: addedProduct.group,
       subcategory: addedProduct.subcategory || null,
+      slug: addedProduct.slug,
     },
     quantity: quantity || 1,
   };
@@ -464,9 +479,16 @@ const decreaseProductQuantity = asyncHandler(async (req, res, next) => {
 });
 
 const getFilteredSearchProducts = asyncHandler(async (req, res, next) => {
-  const searchTerm = req.query.q;
+  const filterTerms = req.query;
 
-  const products = await Product.find()
+  let filteredProducts = [];
+  let brands = [];
+  let groups = [];
+  let subcategories = [];
+  let minPrice = 0;
+  let maxPrice = 0;
+
+  const foundProducts = await Product.find()
     .populate([
       { path: "category", select: "name slug" },
       { path: "subcategory", select: "name slug" },
@@ -477,25 +499,140 @@ const getFilteredSearchProducts = asyncHandler(async (req, res, next) => {
     ])
     .select("-createdAt -updatedAt -__v");
 
-  const searchResults = products.filter((product) => {
-    const searchTerms = searchTerm.toLowerCase().split(" ");
-    return searchTerms.every((term) => {
-      const searchRegex = new RegExp(term, "i");
-      return (
-        product.name.match(searchRegex) ||
-        product.description.match(searchRegex) ||
-        (product.category && product.category.name.match(searchRegex)) ||
-        (product.subcategory && product.subcategory.name.match(searchRegex)) ||
-        (product.group && product.group.name.match(searchRegex)) ||
-        (product.brand && product.brand.name.match(searchRegex)) ||
-        product.specifications.some((spec) =>
-          [spec.type, spec.value].some((field) => field.match(searchRegex))
-        )
-      );
+  if (filterTerms.q) {
+    filteredProducts = foundProducts.filter((product) => {
+      const searchTerms = filterTerms.q.toLowerCase().split(" ");
+      return searchTerms.every((term) => {
+        const searchRegex = new RegExp(term, "i");
+        return (
+          product.name.match(searchRegex) ||
+          product.description.match(searchRegex) ||
+          (product.category && product.category.name.match(searchRegex)) ||
+          (product.subcategory &&
+            product.subcategory.name.match(searchRegex)) ||
+          (product.group && product.group.name.match(searchRegex)) ||
+          (product.brand && product.brand.name.match(searchRegex)) ||
+          product.specifications.some((spec) =>
+            [spec.type, spec.value].some((field) => field.match(searchRegex))
+          )
+        );
+      });
     });
-  });
+    brands = extractBrands(filteredProducts);
+    groups = extractUniqueGroups(filteredProducts);
+    ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+  } else {
+    filteredProducts = foundProducts;
+  }
 
-  res.status(200).json(searchResults);
+  if (filterTerms.sort) {
+    const sortTerm = filterTerms.sort;
+    switch (sortTerm) {
+      case "Price low to high":
+        filteredProducts.sort((a, b) => a.price - b.price);
+        break;
+      case "Price high to low":
+        filteredProducts.sort((a, b) => b.price - a.price);
+        break;
+    }
+  }
+
+  if (filterTerms.category) {
+    const categoryFilter = filterTerms.category;
+
+    filteredProducts = filteredProducts.filter(
+      (product) => categoryFilter === product.category.slug
+    );
+
+    subcategories = extractUniqueSubcategories(filteredProducts);
+    groups = extractUniqueGroups(filteredProducts);
+    brands = extractBrands(filteredProducts);
+    ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+  }
+
+  if (filterTerms.subcategory) {
+    const subcategoryFilter = filterTerms.subcategory;
+    filteredProducts = filteredProducts.filter(
+      (product) => subcategoryFilter === product.subcategory.slug
+    );
+    groups = extractUniqueGroups(filteredProducts);
+    ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+  }
+
+  if (filterTerms.group) {
+    const groupTerms = filterTerms.group.split(",");
+
+    filteredProducts = filteredProducts.filter(
+      (product) =>
+        groupTerms.includes(product.group.name) ||
+        groupTerms.some((term) => term === product.group.slug)
+    );
+    brands = extractBrands(filteredProducts);
+    ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+  }
+
+  if (filterTerms.brand) {
+    const brandTerms = filterTerms.brand.split(",");
+    filteredProducts = filteredProducts.filter((product) =>
+      brandTerms.includes(product.brand.name)
+    );
+    groups = extractUniqueGroups(filteredProducts);
+    ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+  }
+
+  for (const key in filterTerms) {
+    if (
+      Object.hasOwnProperty.call(filterTerms, key) &&
+      key !== "q" &&
+      key !== "price" &&
+      key !== "sort" &&
+      key !== "brand" &&
+      key !== "category" &&
+      key !== "subcategory" &&
+      key !== "group"
+    ) {
+      const specType = key;
+      const specValues = filterTerms[key].split(",");
+
+      if (specValues.length > 0) {
+        filteredProducts = filteredProducts.filter((product) =>
+          product.specifications.some(
+            (spec) => specValues.includes(spec.value) && spec.type === specType
+          )
+        );
+        groups = extractUniqueGroups(filteredProducts);
+        brands = extractBrands(filteredProducts);
+        ({ minPrice, maxPrice } = calculatePriceRange(filteredProducts));
+      }
+    }
+  }
+
+  if (filterTerms.price) {
+    const prices = filterTerms.price.split("-");
+
+    filteredProducts = filteredProducts.filter(
+      (product) =>
+        product.price + 0.01 >= prices[1] && product.price - 0.01 <= prices[3]
+    );
+  }
+
+  const responseData = {
+    products: filteredProducts,
+    brands,
+    groups,
+    minPrice,
+    maxPrice,
+  };
+
+  if (
+    filterTerms.category &&
+    !filterTerms.subcategory &&
+    subcategories.length > 0
+  ) {
+    responseData.subcategories = subcategories;
+  }
+
+  res.status(200).json(responseData);
 });
 
 const getProductData = asyncHandler(async (req, res, next) => {
@@ -543,7 +680,7 @@ const getProductData = asyncHandler(async (req, res, next) => {
     category: foundProduct.category,
     subcategory: foundProduct.subcategory,
     group: foundProduct.group,
-    brand: foundProduct.brand._id,
+    // brand: foundProduct.brand._id,
     _id: { $ne: foundProduct._id },
   })
     .limit(4)
@@ -626,6 +763,169 @@ const getGroupData = asyncHandler(async (req, res, next) => {
   res.status(200).json(products);
 });
 
+const addOrder = asyncHandler(async (req, res, next) => {
+  const { products } = req.body;
+  const { user } = req;
+
+  if (!products) return res.status(404).json({ message: "Products not found" });
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  const cart = await Cart.findOne({
+    user,
+  });
+
+  if (!cart) return res.status(404).json({ message: "Cart not found" });
+
+  const orderItems = products.map((item) => ({
+    product: item._id,
+    quantity: item.quantity || 1,
+  }));
+
+  let amount = 0;
+
+  for (const item of products) {
+    const foundProduct = await Product.findById(item._id);
+
+    if (!foundProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    amount += foundProduct.price * item.quantity || 1;
+
+    const existingIndex = cart.products.findIndex(
+      (product) => product.product === item._id
+    );
+    if (existingIndex !== 1) {
+      cart.products.splice(existingIndex, 1);
+    }
+  }
+
+  await cart.save();
+
+  const updatedCart = await cart.populate({
+    path: "products.product",
+    populate: [
+      { path: "category", select: "name slug" },
+      { path: "subcategory", select: "name slug" },
+      { path: "group", select: "name slug" },
+      { path: "brand", select: "name slug" },
+      { path: "specifications", select: "type value slug" },
+      { path: "images", select: "url" },
+    ],
+    select: "-createdAt -updatedAt -__v",
+  });
+
+  const updatedCartProducts = updatedCart.products.map(
+    ({ product, quantity }) => ({
+      product,
+      quantity,
+    })
+  );
+
+  await Order.create({
+    user,
+    products: orderItems,
+    amount: amount.toFixed(2),
+  });
+
+  res.status(200).json({
+    updatedCart: updatedCartProducts,
+    message: "Your order has been successfully accepted",
+  });
+});
+
+const getRecommendedRecords = asyncHandler(async (req, res, next) => {
+  const orders = await Order.find().select("-__v -createdAt -updatedAt -user");
+
+  const groupCounts = {};
+
+  const groupProducts = {};
+
+  for (const order of orders) {
+    const orderGroups = new Set();
+    for (const item of order.products) {
+      const productId = item.product.toString();
+      const product = await Product.findById(productId).populate("group");
+      const group = product.group;
+      if (group) {
+        orderGroups.add(group._id.toString());
+        if (!groupProducts[group._id.toString()]) {
+          groupProducts[group._id.toString()] = product;
+        }
+      }
+    }
+
+    orderGroups.forEach((groupId) => {
+      groupCounts[groupId] = (groupCounts[groupId] || 0) + 1;
+    });
+  }
+
+  const sortedGroups = Object.entries(groupCounts).sort((a, b) => b[1] - a[1]);
+
+  const top4Groups = sortedGroups.slice(0, 4);
+
+  const recommendedGroups = [];
+  for (const [groupId, count] of top4Groups) {
+    const group = await Group.findById(groupId)
+      .select("name slug category subcategory")
+      .populate("category", "slug")
+      .populate("subcategory", "slug")
+      .lean();
+
+    recommendedGroups.push(group);
+  }
+
+  for (const recommendedGroup of recommendedGroups) {
+    const groupProducts = await Product.find({
+      group: recommendedGroup._id,
+    })
+      .select("images")
+      .populate({
+        path: "images",
+        select: "url",
+      });
+
+    if (groupProducts.length > 0 && groupProducts[0].images.length > 0) {
+      const productImage = groupProducts[0].images[0].url;
+      recommendedGroup.image = productImage;
+    }
+  }
+
+  const productCounts = {};
+
+  for (const order of orders) {
+    for (const item of order.products) {
+      const productId = item.product.toString();
+      productCounts[productId] =
+        (productCounts[productId] || 0) + item.quantity;
+    }
+  }
+
+  const sortedProducts = Object.entries(productCounts).sort(
+    (a, b) => b[1] - a[1]
+  );
+
+  const top4Products = sortedProducts.slice(0, 4);
+
+  const recommendedProducts = [];
+  for (const [productId, quantity] of top4Products) {
+    const product = await Product.findById(productId)
+      .select("-__v -createdAt -updatedAt")
+      .populate([
+        { path: "category", select: "name slug" },
+        { path: "subcategory", select: "name slug" },
+        { path: "group", select: "name slug" },
+        { path: "brand", select: "name slug" },
+        { path: "images", select: "url" },
+        { path: "specifications", select: "type value" },
+      ]);
+    recommendedProducts.push(product);
+  }
+
+  res.status(200).json({ recommendedGroups, recommendedProducts });
+});
+
 module.exports = {
   loginUser,
   registerUser,
@@ -646,4 +946,6 @@ module.exports = {
   getProductData,
   getRecords,
   getGroupData,
+  addOrder,
+  getRecommendedRecords,
 };
