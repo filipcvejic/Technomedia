d1287fc1a972cede38cbf64f301d7d01a69c2164
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-22 01:32:12 +0200

    Added secure field to generate token function

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateToken = (res, id) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
+    secure: true,
     sameSite: "Lax",
     maxAge: 24 * 60 * 60 * 1000,
   });
