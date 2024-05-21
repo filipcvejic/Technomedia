98c7614b972a30468868a82bb278e17a2c181a81
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-21 21:41:53 +0200

    Created wish list actions hook

--- frontend/src/user/hooks/useWishListActions.js
+++ frontend/src/user/hooks/useWishListActions.js
@@ -1,48 +1,54 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
   addToWishList,
-  removeFromWishList,
   moveAllToCart,
+  removeFromWishList,
 } from "../features/wishList/wishListApi";
 
 export function useWishListActions() {
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.userAuth);
 
-  if (userInfo) {
-    const addToWishListHandler = (product) => {
-      dispatch(
-        addToWishList({
-          product: product?._id,
-          name: product.name,
-          description: product.description,
-          price: product.price,
-          images: product.images,
-          category: product.category?._id,
-          subcategory: product.subcategory?._id,
-          group: product.group?._id,
-          brand: product.brand?._id,
-          slug: product.slug,
-        })
-      );
-    };
-
-    const removeFromWishListHandler = (productId) => {
-      dispatch(removeFromWishList({ productId }));
-    };
-
-    const moveAllToCartHandler = (products) => {
-      const adjustedProducts = products.map((product) => product._id);
-
-      console.log(adjustedProducts);
-
-      dispatch(moveAllToCart(adjustedProducts));
-    };
-
+  if (!userInfo) {
     return {
-      addToWishList: addToWishListHandler,
-      removeFromWishList: removeFromWishListHandler,
-      moveAllToCart: moveAllToCartHandler,
+      addToWishList: () => {},
+      removeFromWishList: () => {},
+      moveAllToCart: () => {},
     };
   }
+
+  const addToWishListHandler = (product) => {
+    dispatch(
+      addToWishList({
+        product: product?._id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        images: product.images,
+        category: product.category?._id,
+        subcategory: product.subcategory?._id,
+        group: product.group?._id,
+        brand: product.brand?._id,
+        slug: product.slug,
+      })
+    );
+  };
+
+  const removeFromWishListHandler = (productId) => {
+    dispatch(removeFromWishList({ productId }));
+  };
+
+  const moveAllToCartHandler = (products) => {
+    const adjustedProducts = products.map((product) => product._id);
+
+    console.log(adjustedProducts);
+
+    dispatch(moveAllToCart(adjustedProducts));
+  };
+
+  return {
+    addToWishList: addToWishListHandler,
+    removeFromWishList: removeFromWishListHandler,
+    moveAllToCart: moveAllToCartHandler,
+  };
 }
