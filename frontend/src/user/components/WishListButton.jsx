a16c6599a8f444a3f685e1e2452a4ddc5ef69fbe
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-22 03:16:05 +0200

    Fixed wish list security bug

--- frontend/src/user/components/WishListButton.jsx
+++ frontend/src/user/components/WishListButton.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import HeartIcon from "../svgs/HeartIcon";
 import { useWishListActions } from "../hooks/useWishListActions";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function WishListButton({ exists, product }) {
   const { addToWishList, removeFromWishList } = useWishListActions();
 
+  const navigate = useNavigate();
+
+  const { userInfo } = useSelector((state) => state.userAuth);
+
+  const addProductToWishListHandler = (event) => {
+    event.preventDefault();
+
+    if (!userInfo) {
+      return navigate("/login");
+    }
+
+    if (exists) {
+      removeFromWishList(product._id);
+    } else {
+      addToWishList(product);
+    }
+  };
+
   return (
-    <button
-      className="wish-list-button"
-      onClick={() =>
-        exists ? removeFromWishList(product._id) : addToWishList(product)
-      }
-    >
+    <button className="wish-list-button" onClick={addProductToWishListHandler}>
       <HeartIcon exists={exists} />
     </button>
   );
