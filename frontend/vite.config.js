183348a022a8e31959a31b4024740b711fa68bad
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-23 21:29:41 +0200

    Tried fix some configuration problem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const isProduction = process.env.ENVIRONMENT === "production";
+// const isProduction = process.env.ENVIRONMENT === "production";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    // port: 3000,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
+        target: "https://technomedia-5gpn.onrender.com",
+        // process.env.VITE_API_URL
         changeOrigin: true,
-        secure: isProduction,
+        secure: true,
+        // isProduction
       },
     },
   },
