98979594e3e08d510f232df205cfdb228dc227b4
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-25 23:37:13 +0200

    Finished responsive for forgot password screen

--- frontend/src/user/screens/ForgotPasswordScreen.jsx
+++ frontend/src/user/screens/ForgotPasswordScreen.jsx
@@ -36,32 +36,34 @@ function ForgotPasswordScreen() {
 
   return (
     <div className="forgot-password-container">
-      <h1 className="forgot-password-heading">Forgot password</h1>
-      <form className="forgot-password-form" onSubmit={forgotPasswordHandler}>
-        <div className="form-group">
-          <p>
-            Enter your email address to receive a link to reset your password.
-          </p>
-          <input
-            type="email"
-            placeholder="Enter Your email..."
-            id="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          ></input>
-        </div>
-        <div>
+      <div className="forgot-password-section">
+        <h1 className="forgot-password-heading">Forgot password</h1>
+        <form className="forgot-password-form" onSubmit={forgotPasswordHandler}>
+          <div className="form-group">
+            <p>
+              Enter your email address to receive a link to reset your password.
+            </p>
+            <input
+              type="email"
+              placeholder="Enter Your email..."
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            ></input>
+          </div>
           <div>
-            <button type="submit" className="reset-button">
-              Reset my password
-            </button>
+            <div>
+              <button type="submit" className="reset-button">
+                Reset my password
+              </button>
+            </div>
+            <div className="cancel-button">
+              <Link to="/login">Cancel</Link>
+            </div>
           </div>
-          <div className="cancel-button">
-            <Link to="/login">Cancel</Link>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
