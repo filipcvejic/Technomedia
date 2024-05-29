1e4decf394d01c951156fb1c5f132c2c86d50c7a
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-05-29 23:10:50 +0200

    Created mobile filter list content component

---
+++ frontend/src/user/components/MobileFilterListContent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MobileFilterListContent.css";
+
+function MobileFilterListContent() {
+  return <div>MobileFilterListContent</div>;
+}
+
+export default MobileFilterListContent;
