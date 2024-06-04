f16509ac87c3863624c78d96acfeb5614706582f
filipcvejic <97543155+filipcvejic@users.noreply.github.com>
2024-06-04 22:43:24 +0200

    Tried new admin private route error approach

--- frontend/src/admin/components/AdminPrivateRoute.jsx
+++ frontend/src/admin/components/AdminPrivateRoute.jsx
@@ -2,7 +2,6 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { logout, setCredentials } from "../features/auth/adminAuthSlice";
-import { toast } from "react-toastify";
 
 const AdminPrivateRoute = () => {
   const dispatch = useDispatch();
@@ -10,30 +9,25 @@ const AdminPrivateRoute = () => {
 
   useEffect(() => {
     const checkUser = async () => {
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/api/admin/profile`,
-          {
-            credentials: "include",
-          }
-        );
-
-        const data = await response.json();
-
-        if (!response.ok) {
-          dispatch(logout());
-          navigate("/admin/login");
-          throw new Error(data.message);
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/admin/profile`,
+        {
+          credentials: "include",
         }
+      );
 
-        dispatch(setCredentials({ ...data.admin }));
-      } catch (err) {
-        toast.error(err?.message);
+      const data = await response.json();
+
+      if (!response.ok) {
+        dispatch(logout());
+        navigate("/admin/login");
       }
+
+      dispatch(setCredentials({ ...data.admin }));
     };
 
     checkUser();
-  }, [dispatch, navigate]);
+  }, [dispatch]);
 
   return <Outlet />;
 };
