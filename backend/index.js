1d10528721f89c2b387a8ed095f4f08b5aa22ff4
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-08-04 20:44:03 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,16 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
